8857da51b2f83356297b19119de4cac2576257be
Aniket Raut <rautaniket27@gmail.com>
2021-12-16 20:30:57 +0530

    Update batch_Image_resize.py

--- batch_Image_resize.py
+++ batch_Image_resize.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageOps
 import os
 
-# tool to resize images
+# Script to resize images
 
 path = r'C:\Users\Aniket\Desktop\pro\Version_3.1.2\images\LR' #source path
 spath = r'C:\Users\Aniket\Desktop\pro\Version_3.1.2\images\LR-1' #save path
